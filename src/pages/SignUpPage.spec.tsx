4f6537ca01cfff597519ecdfb48cdf393fe1aff9
JacksonSamuel <jacksonsamu42@gmail.com>
2022-10-12 00:43:15 +0100

    doing sign up page

--- src/pages/SignUpPage.spec.tsx
+++ src/pages/SignUpPage.spec.tsx
@@ -1,13 +1,70 @@
 import "@testing-library/jest-dom"
 import React from "react";
 import {render} from '@testing-library/react'
+import userEvent from "@testing-library/user-event"
 import {describe, expect, it} from 'vitest'
 import SignUpPage from "./SignUpPage";
 
-describe("signUp", () => {
-    it('has header', () => {
-        const screen = render(<SignUpPage/>)
-        const headerElement = screen.queryByRole('heading', {name: "Sign Up"})
-        expect(headerElement).toBeInTheDocument()
-    });
+describe("Sign up page", () => {
+    describe("Layout", () => {
+        it('has header', () => {
+            const screen = render(<SignUpPage/>)
+            const headerElement = screen.getByRole('heading', {name: /sign up/i})
+            expect(headerElement).toBeInTheDocument()
+        });
+
+        it('has username input', () => {
+            const screen = render(<SignUpPage/>)
+
+            const usernameInput = screen.getByRole("textbox", {name: /username/i})
+            expect(usernameInput).toBeInTheDocument()
+        });
+
+        it('has email input', () => {
+            const screen = render(<SignUpPage/>)
+
+            const emailInput = screen.getByRole("textbox", {name: /email/i})
+            expect(emailInput).toBeInTheDocument()
+        });
+
+        it('has password input and password repeat', () => {
+            const screen = render(<SignUpPage/>)
+
+            const password = screen.getByPlaceholderText("password")
+            expect(password).toBeInTheDocument()
+            
+            const passwordRepeat = screen.getByPlaceholderText("password-repeat")
+            expect(passwordRepeat).toBeInTheDocument()
+        });
+
+        it('has sign up button', () => {
+            const screen = render(<SignUpPage/>)
+
+            const loginButton = screen.queryByRole("button", {name: /sign up/i})
+            expect(loginButton).toBeInTheDocument()
+        });
+
+        it('disables sign up button initially', () => {
+            const screen = render(<SignUpPage/>)
+
+            const loginButtonDisable = screen.queryByRole("button", {name: /sign up/i})
+            expect(loginButtonDisable).toBeDisabled()
+        });
+    })
+
+    describe("Interactions", () => {
+        it('enables the button when password and password repeat fields have same value', async() => {
+            const user = userEvent.setup()
+            const screen = render(<SignUpPage/>)
+
+            const passwordInput = screen.getByPlaceholderText("password")
+            const passwordRepeatInput = screen.getByPlaceholderText("password-repeat")
+
+            await user.type(passwordInput, "same-password")
+            await user.type(passwordRepeatInput, "same-password")
+
+            const loginButton = screen.queryByRole("button", {name: /sign up/i})
+            expect(loginButton).toBeEnabled()
+        });
+    })
 })
--- src/pages/SignUpPage.tsx
+++ src/pages/SignUpPage.tsx
@@ -1,8 +1,44 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 const SignUpPage = () => {
+  const [disabled, setDisabled] = useState(true)
+  const [password, setPassword] = useState("")
+  const [passwordRepeat, setPasswordRepeat] = useState("")
+
+  const onChangePassword = ({target}: React.ChangeEvent<HTMLInputElement>) => {
+    const currentValue = target.value
+
+    setPassword(currentValue)
+    setDisabled(currentValue !== passwordRepeat)
+  }
+
+  const onChangePasswordRepeat = ({target}: React.ChangeEvent<HTMLInputElement>) => {
+    const currentValue = target.value
+
+    setPasswordRepeat(currentValue)
+    setDisabled(currentValue !== password)
+  }
+
   return (
-    <h1>Sign Up</h1>
+    <div>
+      <h1>Sign Up</h1>
+      
+      <form>
+        <label htmlFor="email">Email</label>
+        <input type="email" name='email' id='email' placeholder='email'/>
+
+        <label htmlFor="username">Username</label>
+        <input type="text" name='username' id='username' placeholder='username'/>
+        
+        <label htmlFor="password">Password</label>
+        <input type="password" name='password' id='password' placeholder='password' value={password} onChange={onChangePassword}/>
+
+        <label htmlFor="password-repeat">Password Repeat</label>
+        <input type="password" name='password-repeat' id='password-repeat' placeholder='password-repeat' value={passwordRepeat} onChange={onChangePasswordRepeat}/>
+
+        <button disabled={disabled}>Sign Up</button>
+      </form>
+    </div>
   )
 }
 
